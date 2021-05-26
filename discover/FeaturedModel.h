cf18371af346191a095daf10c8316cc892baacec
Carl Schwan <carl@carlschwan.eu>
2021-05-26 20:41:34 +0200

    Fix unrelated change

--- discover/FeaturedModel.h
+++ discover/FeaturedModel.h
@@ -2,7 +2,7 @@
  *   SPDX-FileCopyrightText: 2016 Aleix Pol Gonzalez <aleixpol@blue-systems.com>
  *   SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
  *
- *   SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicnseRef-KDE-Accepted-GPL
+ *   SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
 
 #ifndef FEATUREDMODEL_H
